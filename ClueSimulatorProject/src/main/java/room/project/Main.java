724d742091bc961ab866e9f4b7d742fbac865246
Sartaj <78692sartaj@gmail.com>
2023-01-16 13:31:59 +0530

    Added Clue Simulator

---
+++ ClueSimulatorProject/src/main/java/room/project/Main.java
@@ -0,0 +1,13 @@
+package room.project;
+
+public class Main {
+    /**
+     * Driver method for random guessing approach
+     *
+     * @param args not used for driver
+     */
+    public static void main(String[] args) {
+        DetectiveJill.main();
+
+    }
+}
